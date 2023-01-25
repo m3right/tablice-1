d5141bee1689b8745584b77fd2082d0a77ea1552
Ania Dermin <66634563+m3right@users.noreply.github.com>
2023-01-25 20:43:01 +0000

    1. zadanie 1 - dodanie int los do wpisanej liczby 2. zadanie 2 - wypisanie w tablicy potęg cyfry 2

--- Main.java
+++ Main.java
@@ -1,12 +1,26 @@
 import java.io.*;
+import java.util.Random;
+import static java.lang.Math.*;
 
 public class Main {
 
-  public static void wczytaj_tablice(int[] tablica, int rozmiar) throws IOException {
+  public static void wczytaj_tablice_zadanie1(int[] tablica, int rozmiar) throws IOException {
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    Random rnd = new Random();
+    int los;
+    
     for (int i = 0; i < rozmiar; i++) {
+      los = rnd.nextInt(101);
+      System.out.println("Wylosowana liczba:"+los);
       System.out.println("Podaj liczbe: ");
-      tablica[i] = Integer.parseInt(in.readLine());
+      
+      
+      tablica[i] = Integer.parseInt(in.readLine())+los;
+    }
+  }
+  public static void wczytaj_tablice_zadanie2(int[] tablica, int rozmiar) throws IOException {
+    for (int i = 0; i < rozmiar; i++) {
+      tablica[i] = (int)pow(2, i);
     }
   }
 
@@ -22,7 +36,21 @@ public class Main {
       System.out.println("Podaj rozmiar: ");
       r = Integer.parseInt(in.readLine());
       int[] t = new int[r];
-      wczytaj_tablice(t, r);
+      wczytaj_tablice_zadanie1(t, r);
+
+      System.out.println("Podales nastepujace liczby:");
+      wypisz_tablice(t, r);
+
+    } catch (IOException e)
+      {
+    }
+    try {
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      int r;
+      System.out.println("Podaj rozmiar: ");
+      r = Integer.parseInt(in.readLine());
+      int[] t = new int[r];
+      wczytaj_tablice_zadanie2(t, r);
 
       System.out.println("Podales nastepujace liczby:");
       wypisz_tablice(t, r);
@@ -34,7 +62,7 @@ public class Main {
 
 /*
  * Cwiczenia: 1. Zamiast wczytywac elementy tablicy, wylosuj je z przedzialu od
- * 0 do 100, a nastepnie do wartosci podanej przez uzytkownika. 2. Zamiast
- * wczytywac elementy tablicy, wypelnij ja kolejnymi liczbami calkowitymi,
+ * 0 do 100, a dodaj nastepnie do wartosci podanej przez uzytkownika. 
+2. Zamiast wczytywac elementy tablicy, wypelnij ja kolejnymi liczbami calkowitymi,
  * kolejnymi potegami dwojki itp.
  */
